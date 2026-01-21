784dd7897932e23390065db7f26846dca090967f
z4lthor <z4lthor@gmail.com>
2026-01-21 23:58:56 +0100

    Modify toggle-monitor.sh to set relative position

--- os/linux/xorg/toggle-monitor.sh
+++ os/linux/xorg/toggle-monitor.sh
@@ -5,16 +5,44 @@
 #
 
 if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 INPUT"
+    echo "Usage: $0 INPUT [POSITION]"
     exit 1
 fi
 
 INPUT=$1
+POSITION=$2
 
 if ! xrandr --query | grep "connected" | grep -q "^$INPUT"; then
     echo "Error: Input $INPUT not found or connected"
     exit 1
 fi
+
+if [[ -z "$POSITION" ]]; then
+    POSITION="right"
+fi
+
+case "$POSITION" in
+  left)
+    POSITION_OPT="--left-of"
+    ;;
+  right)
+    POSITION_OPT="--right-of"
+    ;;
+  above)
+    POSITION_OPT="--above"
+    ;;
+  below)
+    POSITION_OPT="--below"
+    ;;
+  same-as)
+    POSITION_OPT="--same-as"
+    ;;
+  *)
+    echo "Invalid position: $POSITION"
+    echo "Available positions: left, right, above, below, same-as"
+    exit 1
+    ;;
+esac
 
 PRIMARY=$(xrandr --query | awk '/ primary / { print $1 }')
 
@@ -23,7 +51,7 @@ if xrandr --query | grep "^$INPUT" | grep -qE "[0-9]+x[0-9]+\+[0-9]+\+[0-9]+"; t
     xrandr --output $INPUT --off
 else
     echo "Turning on monitor on $INPUT"
-    xrandr --output $INPUT --auto --right-of $PRIMARY
+    xrandr --output $INPUT --auto $POSITION_OPT $PRIMARY
 fi
 
 exit 0
