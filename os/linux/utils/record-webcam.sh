682f61f4f1b26998d2e49550b8c2db772878038e
z4lthor <z4lthor@gmail.com>
2026-02-25 23:58:59 +0100

    Modify record-webcam.sh. Refactoring

--- os/linux/utils/record-webcam.sh
+++ os/linux/utils/record-webcam.sh
@@ -4,12 +4,11 @@
 # Author: z4lthor <z4lthor@gmail.com>
 #
 
-DEVICE="${1:-/dev/video0}"
-RES="${2:-1280x720}"
-FPS="${3:-30}"
-DURATION="${4:-0}"
-
-OUTPUT="rec_$(date +%Y%m%d_%H%M%S).mp4"
+DEVICE="/dev/video0"
+RES="1280x720"
+FPS=30
+OUTPUT="${1:-output_$(date +%Y%m%d_%H%M%S).mp4}"
+DURATION="${2:-0}"
 
 echo "Device: $DEVICE"
 echo "Resolution: $RES"
@@ -17,19 +16,16 @@ echo "FPS: $FPS"
 echo "Duration: ${DURATION}s (0=infinity)"
 echo "Output: $OUTPUT"
 
-WIDTH=$(echo $RES | cut -d'x' -f1)
-HEIGHT=$(echo $RES | cut -d'x' -f2)
-
 if [ "$DURATION" = "0" ]; then
     echo "Recording (Ctrl+C to stop)..."
-    ffmpeg -f v4l2 -framerate $FPS -video_size $RES -i $DEVICE \
+    ffmpeg -f v4l2 -framerate "$FPS" -video_size "$RES" -i "$DEVICE" \
         -c:v libx264 -pix_fmt yuv420p \
-        -preset veryfast -crf 21 $OUTPUT
+        -preset veryfast -crf 25 "$OUTPUT"
 else
     echo "Recording $DURATION seconds..."
     ffmpeg -f v4l2 -framerate $FPS -video_size $RES -i $DEVICE \
         -t $DURATION -c:v libx264 -pix_fmt yuv420p \
-        -preset veryfast -crf 21 $OUTPUT
+        -preset veryfast -crf 25 $OUTPUT
 fi
 
 echo "Ready on $OUTPUT"
