8bcb496de3eeb1ccf942b2ecfe178d398a8fb943
z4lthor <z4lthor@gmail.com>
2026-02-26 23:49:58 +0100

    Modify record-screen.sh. Allow selecting a specific monitor as the input source

--- os/linux/utils/record-screen.sh
+++ os/linux/utils/record-screen.sh
@@ -4,13 +4,31 @@
 # Author: z4lthor <z4lthor@gmail.com>
 #
 
+BIN=/usr/bin/ffmpeg
 FMT="x11grab"
 VCODEC="libx264"
 CRF=25
 PRESET="veryfast"
 PIXFMT="yuv420p"
 FPS=30
-OUTPUT=${1:-output-$(date +%Y-%m-%d_%H-%M-%S).mp4}
+FOCUS=false
+
+help() {
+cat <<EOF
+Usage:
+$0 [OPTION]... [OUTPUT]
+
+Options:
+-m, --monitor MONITOR       Record monitor MONITOR
+-f, --focus                 Record focused window
+EOF
+}
+
+check_command() {
+    local bin="$1"
+
+    command -v "$bin" > /dev/null 2>&1
+}
 
 confirm_action() {
     while true; do
@@ -29,13 +47,67 @@ confirm_action() {
     done
 }
 
+if ! check_command $BIN; then
+    error "$BIN not found"
+    exit 1
+fi
+
+OPTS=$(getopt -o m:fh \
+    --long monitor:,focus,help \
+    -n "$0" -- "$@")
+
+if [[ $? -ne 0 ]]; then
+    help
+    exit 1
+fi
+
+eval set -- "$OPTS"
+
+while true; do
+    case "$1" in
+        -m|--monitor)
+            MONITOR="$2"
+            shift 2
+            ;;
+        -f|--focus)
+            FOCUS=true
+            shift
+            ;;
+        -h|--help)
+            help
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            error "Internal getopt error"
+            exit 1
+            ;;
+    esac
+done
+
+OUTPUT=${1:-screen_output_$(date +%Y-%m-%d_%H-%M-%S).mp4}
+
 if [[ -f "$OUTPUT" ]] && ! confirm_action "Do you want to overwrite the file $(basename "$OUTPUT")?"; then
     echo "Recording aborted."
     exit 0
 fi
 
-read -r X Y W H < <(xdotool getwindowfocus getwindowgeometry --shell | \
-    sed -n 's/^X=\([0-9]*\).*/\1/p;s/^Y=\([0-9]*\).*/\1/p;s/^WIDTH=\([0-9]*\).*/\1/p;s/^HEIGHT=\([0-9]*\).*/\1/p' | xargs)
+if [[ -n "$MONITOR" ]]; then
+    read -r W H X Y < <(xrandr --query | grep "^$MONITOR" | grep -oP '\d+x\d+\+\d+\+\d+' | tr 'x+' ' ')
+
+    if [ -z "$W" ]; then
+        echo "Error: Not found monitor $MONITOR"
+        exit 1
+    fi
+fi
+
+if [[ "$FOCUS" = "true" ]]; then
+    read -r X Y W H < <(xdotool getwindowfocus getwindowgeometry --shell | \
+        sed -n 's/^X=\([0-9]*\).*/\1/p;s/^Y=\([0-9]*\).*/\1/p;s/^WIDTH=\([0-9]*\).*/\1/p;s/^HEIGHT=\([0-9]*\).*/\1/p' | xargs)
+fi
 
 ffmpeg \
   -video_size "${W}x${H}" \
