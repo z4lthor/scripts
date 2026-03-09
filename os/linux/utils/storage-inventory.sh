80b42eee5aa4b22562cceb68c107ca44b0f8de8e
z4lthor <z4lthor@gmail.com>
2026-03-09 20:32:48 +0100

    Modify storage-inventory.sh. Add storage device inventory with root checks and mount valitation

--- os/linux/utils/storage-inventory.sh
+++ os/linux/utils/storage-inventory.sh
@@ -4,19 +4,48 @@
 # Author: z4lthor <z4lthor@gmail.com>
 #
 
+if [[ $EUID -ne 0 ]]; then
+    echo "Error: Must be root" >&2
+    exit 1
+fi
+
 if [[ "$#" -ne 2 ]]; then
-    echo "Usage: $(basename "$0") PATH OUTPUT"
+    echo "Usage: $(basename "$0") DEVICE OUTPUT" >&2
     exit 1
 fi
 
-SRCPATH=$(realpath -m "$1")
-DSTFILE=$(realpath -m "$2")
+DEVICE="$1"
+OUTPUT="$2"
 
-if [[ ! -d "$SRCPATH" ]];then
-    echo "Source path is invalid: $SRCPATH"
+if [[ ! -b "$DEVICE" ]]; then
+    echo "Error: Device $DEVICE not found" >&2
     exit 1
 fi
 
-find $SRCPATH -printf "%M %u %g %s %t %p\n" > $DSTFILE
+OUTPUT_DIR=$(dirname "$OUTPUT")
+
+if [[ ! -d "$OUTPUT_DIR" ]]; then
+    echo "Error: Output directory $OUTPUT_DIR does not exists" >&2
+    exit 1
+fi
+
+MOUNTPOINT=$(findmnt -no TARGET "$DEVICE")
+
+if [[ -z "$MOUNTPOINT" ]] || ! mountpoint -q "$MOUNTPOINT"; then
+    echo "Error: Device $DEVICE not mounted" >&2
+    exit 1
+fi
+
+echo -n "Making $DEVICE inventory ... "
+
+if find "$MOUNTPOINT" -printf "%M %u %g %s %t %p\n" > "$OUTPUT"; then
+    echo "OK"
+    echo
+    echo "Output: $OUTPUT"
+    echo "Entries: $(wc -l < "$OUTPUT")"
+else
+    echo "Fail" >&2
+    exit 1
+fi
 
 exit 0
