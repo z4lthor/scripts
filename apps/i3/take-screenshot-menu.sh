222b8973c257f2b7b58eac36bdd7689f9da010c6
z4lthor <z4lthor@gmail.com>
2026-03-14 22:15:54 +0100

    Add take-screenshot-menu.sh. Take a screenshot and send a notification

---
+++ apps/i3/take-screenshot-menu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# A simple power management script for i3wm using rofi.
+# Author: z4lthor <z4lthor@gmail.com>
+#
+
+DEST_DIR=$HOME/Pictures/Screenshots
+ID=1001
+
+if FILE=$(take-screenshot "$DEST_DIR"); then
+    dunstify -r $ID \
+             -a "Scrot" \
+             -i "$FILE" \
+             "Snapshot Taken" \
+             "File: $(basename "$FILE")\nLocation: $DEST_DIR" \
+             --action="open,Open Folder"
+else
+    dunstify -u critical "Error" "Failed to take screenshot"
+fi
