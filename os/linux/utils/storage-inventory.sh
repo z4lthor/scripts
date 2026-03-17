842e058d2109c3a0823a7750248f996b026fb3e9
z4lthor <z4lthor@gmail.com>
2026-03-17 19:43:29 +0100

    Modify storage-inventory.sh. Display physical device, device model and device size

--- os/linux/utils/storage-inventory.sh
+++ os/linux/utils/storage-inventory.sh
@@ -4,6 +4,48 @@
 # Author: z4lthor <z4lthor@gmail.com>
 #
 
+get_physical_device() {
+    local dev="$1"
+    local node_name
+    local slave
+    local physical_name
+
+    [[ ! -b "$dev" ]] && return 1
+
+    node_name=$(basename "$(readlink -f "$dev")")
+
+    if [[ -d "/sys/class/block/$node_name/slaves" ]] && [[ "$(ls -A "/sys/class/block/$node_name/slaves")" ]]; then
+        slave=$(ls -1 "/sys/class/block/$node_name/slaves" | head -n1)
+        physical_name=$(lsblk -dno PKNAME "/dev/$slave" 2>/dev/null | xargs)
+        [[ -z "$physical_name" ]] && physical_name="$slave"
+    else
+        physical_name=$(lsblk -dno PKNAME "/dev/$node_name" 2>/dev/null | xargs)
+        [[ -z "$physical_name" ]] && physical_name="$node_name"
+    fi
+
+    echo "/dev/$physical_name"
+}
+
+get_device_model() {
+    local dev="$1"
+
+    [[ ! -b "$dev" ]] && return 1
+
+    device_model=$(smartctl -i "$dev" | grep -E "Model Number|Device Model" | cut -d: -f2 | xargs | tr ' ' '-')
+
+    echo "$device_model"
+}
+
+get_device_size() {
+    local dev="$1"
+
+    [[ ! -b "$dev" ]] && return 1
+
+    device_size=$(smartctl -i "$dev" | grep "User Capacity" | grep -oP '\[\K[^\]]+' | xargs | tr ' ' '-')
+
+    echo "$device_size"
+}
+
 if [[ $EUID -ne 0 ]]; then
     echo "Error: Must be root" >&2
     exit 1
@@ -15,6 +57,9 @@ if [[ "$#" -ne 2 ]]; then
 fi
 
 DEVICE="$1"
+PHYSICAL_DEVICE=$(get_physical_device "$DEVICE")
+DEVICE_MODEL=$(get_device_model "$PHYSICAL_DEVICE")
+DEVICE_SIZE=$(get_device_size "$PHYSICAL_DEVICE")
 OUTPUT="$2"
 
 if [[ ! -b "$DEVICE" ]]; then
@@ -36,13 +81,18 @@ if [[ -z "$MOUNTPOINT" ]] || ! mountpoint -q "$MOUNTPOINT"; then
     exit 1
 fi
 
-echo -n "Making $DEVICE inventory ... "
+echo "Details"
+echo "Device: $PHYSICAL_DEVICE"
+echo "Model:  $DEVICE_MODEL"
+echo "Size:   $DEVICE_SIZE"
+echo "Output: $OUTPUT"
+
+echo -n "Making inventory ... "
 
 if find "$MOUNTPOINT" -xdev -printf "%i|%y|%m|%u|%g|%s|%A@|%p|%l\0" > "$OUTPUT"; then
     echo "OK"
     echo
     echo "Output: $OUTPUT"
-    echo "Entries: $(wc -l < "$OUTPUT")"
 else
     echo "Fail" >&2
     exit 1
