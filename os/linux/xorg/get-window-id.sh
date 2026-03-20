dc39508f8d27050fd48bc3efe842a2b7a61a3015
z4lthor <z4lthor@gmail.com>
2026-03-20 19:02:35 +0100

    Modify get-window-id.sh. Fix display error issue

--- os/linux/xorg/get-window-id.sh
+++ os/linux/xorg/get-window-id.sh
@@ -7,7 +7,7 @@
 set -euo pipefail
 
 if [[ -z "$DISPLAY" ]] || ! xset q >/dev/null 2>&1; then
-    echo "Error: X server is down or not available"
+    echo "Error: X server is down or not available" >&2
     exit 1
 fi
 
