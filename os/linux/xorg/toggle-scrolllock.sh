e78933bacf804ca28cd5f9d0d808fb19e8f72836
z4lthor <z4lthor@gmail.com>
2026-04-08 20:16:37 +0200

    Modify toggle-scrolllock.sh. Check if X server is running

--- os/linux/xorg/toggle-scrolllock.sh
+++ os/linux/xorg/toggle-scrolllock.sh
@@ -12,6 +12,11 @@ turnoff() {
     xset -led 3
 }
 
+if [[ -z "$DISPLAY" ]] || ! xset q >/dev/null 2>&1; then
+    echo "Error: X server is down or not available" >&2
+    exit 1
+fi
+
 ACTION="$1"
 
 if [[ -n "$ACTION" ]]; then
