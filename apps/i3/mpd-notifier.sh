f9ea6be01f6f622bd0ba1024af215840c7abe543
z4lthor <z4lthor@gmail.com>
2026-04-13 21:19:15 +0200

    Modify mpd-notifier.sh. Add restart logic and playing, paused and stopped message notifications

--- apps/i3/mpd-notifier.sh
+++ apps/i3/mpd-notifier.sh
@@ -4,21 +4,24 @@
 # Author: z4lthor <z4lthor@gmail.com>
 #
 
-mpc idleloop player | while read -r _; do
-    STATE=$(mpc status %state%)
-    echo "$STATE"
+BIN=/usr/bin/mpc
 
-    if [ "$STATE" == "playing" ]; then
-        TITLE=$(mpc current -f "%title%")
-        ARTIST=$(mpc current -f "%artist%")
+while true; do
+    $BIN idleloop player | while read -r _; do
+        STATE=$($BIN status %state%)
 
-        [ -z "$TITLE" ] && TITLE=$(mpc current -f "%file%")
+        if [ "$STATE" == "playing" ]; then
+            TITLE=$($BIN current -f "%title%")
+            ARTIST=$($BIN current -f "%artist%")
+            [ -z "$TITLE" ] && TITLE=$($BIN current -f "%file%")
+            
+            dunstify -u low -r 9910 -i audio-speakers "Now Playing:" "<b>$TITLE</b>\n$ARTIST"
+        elif [ "$STATE" == "paused" ]; then
+            dunstify -u low -r 9910 -i audio-speakers "Playback Paused"
+        elif [ "$STATE" == "stopped" ]; then
+            dunstify -u low -r 9910 -i audio-speakers "Playback Stopped"
+        fi
+    done
 
-        dunstify -u low -r 9910 -i audio-speakers "Now Playing:" "<b>$TITLE</b>\n$ARTIST"
-
-    elif [ "$STATE" == "paused" ]; then
-        dunstify -u low -r 9910 -i audio-speakers "Playback Paused"
-    elif [ "$STATE" == "stopped" ]; then
-        dunstify -u low -r 9910 -i audio-speakers "Playback Stopped"
-    fi
+    sleep 2
 done
