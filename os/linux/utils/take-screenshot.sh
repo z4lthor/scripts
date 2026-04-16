b981e6d8944785029ef7096225700ca93d718df7
z4lthor <z4lthor@gmail.com>
2026-04-16 17:49:03 +0200

    Modify take-screenshot.sh. Add getop to parse CLI options

--- os/linux/utils/take-screenshot.sh
+++ os/linux/utils/take-screenshot.sh
@@ -5,12 +5,51 @@
 #
 
 DEFDIR=$HOME/Pictures/Screenshots
-DSTDIR=${1:-$DEFDIR}
+
+help() {
+cat <<EOF
+Usage:
+$(basename "$0") [OPTION]... [DIRECTORY]
+
+Options:
+-h, --help  Show this help
+EOF
+}
 
 if [[ -z "$DISPLAY" ]] || ! xset q >/dev/null 2>&1; then
     echo "Error: X server is down or not available" >&2
     exit 1
 fi
+
+OPTS=$(getopt -o h \
+    --long help \
+    -n "$0" -- "$@")
+
+if [[ $? -ne 0 ]]; then
+    help
+    exit 1
+fi
+
+eval set -- "$OPTS"
+
+while true; do
+    case "$1" in
+        -h|--help)
+            help
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            error "Internal getopt error"
+            exit 1
+            ;;
+    esac
+done
+
+DSTDIR=${1:-$DEFDIR}
 
 if [[ ! -d "$DSTDIR" ]]; then
     echo "Directory $DSTDIR does not exists. Creating it..."
