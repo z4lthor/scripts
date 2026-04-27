d20d89a53cce69416131bd9a8f1cad59d44ed25a
z4lthor <z4lthor@gmail.com>
2026-04-27 19:33:19 +0200

    Modify packages-by-size.sh. Makes output optional and refactors code

--- os/linux/utils/packages-by-size.sh
+++ os/linux/utils/packages-by-size.sh
@@ -4,14 +4,32 @@
 # Author: z4lthor <z4lthor@gmail.com>
 #
 
-if [[ $# -eq 0 ]]; then
-    echo "Usage $(basename "$0") <OUTPUT>"
+BIN=/usr/bin/pacman
+
+if ! command -v "$BIN" > /dev/null 2>&1; then
+    echo "$BIN not found"
     exit 1
 fi
 
-OUTPUT="$1"
+if [[ $# -gt 1 ]]; then
+    echo "Usage $(basename "$0") [OUTPUT]"
+    exit 1
+fi
 
-pacman -Qie | grep -E '^(Name|Installed Size)' | cut -d: -f2 | paste -d' ' - - | awk '{
+OUTPUT=${1:-./$(hostname)_packages_$(date +%Y-%m-%d_%H-%M-%S).txt}
+DSTDIR=$(dirname "$OUTPUT")
+
+if [[ ! -d "$DSTDIR" ]]; then
+    echo "Error: Directory $DSTDIR not exists"
+    exit 1
+fi
+
+if [[ ! -w "$DSTDIR" ]]; then
+    echo "Error: Directory $DSTDIR not writable"
+    exit 1
+fi
+
+"$BIN" -Qie | grep -E '^(Name|Installed Size)' | cut -d: -f2 | paste -d' ' - - | awk '{
     if($3=="MiB") {val=$2*1024} 
     else if($3=="GiB") {val=$2*1024*1024} 
     else {val=$2}
