8ced4ba77c9a6fdfe0e13608a07af96126230270
z4lthor <z4lthor@gmail.com>
2026-05-18 18:14:41 +0200

    Modify toggle-monitor-menu.sh. Add video output connection check

--- apps/i3/toggle-monitor-menu.sh
+++ apps/i3/toggle-monitor-menu.sh
@@ -12,11 +12,21 @@ check_command() {
     command -v "$bin" > /dev/null 2>&1
 }
 
+is_connected() {
+    local output=$1
+    xrandr --query | grep -q "^$output connected"
+}
+
 confirm() {
     echo -e "󰄬 Yes\n󰏐 No" | rofi -dmenu -i -p "Confirm $1?" \
         -theme-str 'window {width: 25%;} listview {lines: 2;}'
 }
 
 ! check_command toggle-monitor && exit 1
+
+if ! is_connected "$OUTPUT"; then
+    dunstify -u critical -t 2000 "Output $OUTPUT is disconnected"
+    exit 1
+fi
 
 [[ $(confirm "Toggle Monitor") == *"Yes" ]] && toggle-monitor "$OUTPUT"
