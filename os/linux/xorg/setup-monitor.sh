c8176db661dfb721fca82346457ebb53d7d27a9c
z4lthor <z4lthor@gmail.com>
2026-05-22 17:43:16 +0200

    Add setup-monitor.sh. Configure DPMS and screensaver settings for the current X session

---
+++ os/linux/xorg/setup-monitor.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Configure DPMS and screensaver settings for the current X session
+# Author: z4lthor <z4lthor@gmail.com>
+#
+
+STANDBY=600
+SUSPEND=800
+OFF=1000
+SCREENSAVER_DELAY_SEC=300
+
+if [[ $# -gt 1 ]]; then
+    echo "Usage: ${0##*/} [MODE]"
+    exit 1
+fi
+
+MODE=${1-normal}
+
+case "$MODE" in
+    normal)
+        xset dpms $STANDBY $SUSPEND $OFF
+        xset s "$SCREENSAVER_DELAY_SEC"
+        xset s noblank
+        ;;
+    always-on)
+        xset -dpms
+        xset s off
+        ;;
+    *)
+        echo "Error: Invalid mode. Modes=[normal|always-on]" >&2
+        exit 1
+        ;;
+esac
+
+echo "Set mode to $MODE"
