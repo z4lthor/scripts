1641a6c35e806896eb983db0166da641b724f00c
z4lthor <z4lthor@gmail.com>
2026-05-23 19:09:42 +0200

    Modify setup-monitor.sh. Add eco mode with standby, suspend, off and screensaver delays set to 50% of normal mode

--- os/linux/xorg/setup-monitor.sh
+++ os/linux/xorg/setup-monitor.sh
@@ -9,6 +9,11 @@ SUSPEND=800
 OFF=1000
 SCREENSAVER_DELAY_SEC=300
 
+STANDBY_ECO=$((STANDBY / 2))
+SUSPEND_ECO=$((SUSPEND / 2))
+OFF_ECO=$((OFF / 2))
+SCREENSAVER_ECO_DELAY_SEC=$((SCREENSAVER_DELAY_SEC / 2))
+
 if [[ $# -gt 1 ]]; then
     echo "Usage: ${0##*/} [MODE]"
     exit 1
@@ -22,12 +27,17 @@ case "$MODE" in
         xset s "$SCREENSAVER_DELAY_SEC"
         xset s noblank
         ;;
+    eco)
+        xset dpms $STANDBY_ECO $SUSPEND_ECO $OFF_ECO
+        xset s "$SCREENSAVER_ECO_DELAY_SEC"
+        xset s blank
+        ;;
     always-on|presentation)
         xset -dpms
         xset s off
         ;;
     *)
-        echo "Error: Invalid mode. Modes=[normal|always-on|presentation]" >&2
+        echo "Error: Invalid mode. Modes=[normal|eco|always-on|presentation]" >&2
         exit 1
         ;;
 esac
