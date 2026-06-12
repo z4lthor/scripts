840d7098012e00905977f8ccc3a40546dc2ec4c0
z4lthor <z4lthor@gmail.com>
2026-06-12 18:37:02 +0200

    Modify setup-monitor.sh. Check if X server is running.

--- os/linux/xorg/setup-monitor.sh
+++ os/linux/xorg/setup-monitor.sh
@@ -39,6 +39,11 @@ is_positive_integer() {
     [[ "$num" =~ ^[1-9][0-9]*$ ]]
 }
 
+if [[ -z "$DISPLAY" ]] || ! xset q >/dev/null 2>&1; then
+    echo "Error: X server is down or not available" >&2
+    exit 1
+fi
+
 if [[ $# -gt 1 && $# -lt 5 ]] || [[ $# -gt 5 ]]; then
     help
     exit 1
