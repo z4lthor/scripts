3b54ce16edcab9aedaef7c1bfe30a1b2b6e1a4fd
z4lthor <z4lthor@gmail.com>
2026-06-12 17:21:44 +0200

    Add weather.sh. Fetches current weather information for your location

---
+++ os/linux/utils/weather.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Fetches current weather information for your location
+# Author: z4lthor <z4lthor@gmail.com>
+#
+
+DATA=$(curl -s 'https://wttr.in/?format=j1')
+
+jq -c '{
+    temp: .current_condition[0].temp_C,
+    desc: .current_condition[0].weatherDesc[0].value,
+}' <<< "$DATA"
